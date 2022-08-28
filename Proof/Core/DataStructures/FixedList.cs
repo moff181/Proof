f8118d74e47c68a00a05841d311e7afe8a7ad59d
will181 <willmoffat181@gmail.com>
2022-08-28 20:26:55 +0100

    Added FixedList

---
+++ Proof/Core/DataStructures/FixedList.cs
@@ -0,0 +1,30 @@
+﻿namespace Proof.Core.DataStructures
+{
+    internal class FixedList<T> where T : unmanaged
+    {
+        public FixedList(int size)
+        {
+            Items = new T[size];
+            Index = 0;
+        }
+
+        public T[] Items { get; }
+
+        public int Index { get; private set; }
+
+        public void Add(T[] toAdd)
+        {
+            Array.Copy(toAdd, 0, Items, Index, toAdd.Count());
+        }
+
+        public int Capacity()
+        {
+            return Items.Length;
+        }
+
+        public void Clear()
+        {
+            Index = 0;
+        }
+    }
+}
