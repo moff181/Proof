b8aeeed1fdfda166c9d3f96dc5fd2f4db1a59318
will181 <willmoffat181@gmail.com>
2022-08-28 20:40:31 +0100

    Added VertexLayout

---
+++ Proof/Render/Buffers/VertexLayout.cs
@@ -0,0 +1,32 @@
+﻿namespace Proof.Render.Buffers
+{
+    internal class VertexLayout
+    {
+        private readonly IList<int> _arraySizes;
+
+        public VertexLayout()
+        {
+            _arraySizes = new List<int>();
+        }
+
+        public void AddArray(int arraySize)
+        {
+            _arraySizes.Add(arraySize);
+        }
+
+        public int Count()
+        {
+            return _arraySizes.Count;
+        }
+
+        public int SumOfElements()
+        {
+            return _arraySizes.Sum();
+        }
+
+        public int Stride()
+        {
+            return SumOfElements() * sizeof(float);
+        }
+    }
+}
