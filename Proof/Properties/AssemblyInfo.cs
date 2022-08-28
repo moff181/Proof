2c70896b1a136d4f6aea5d0880b82be635747c38
will181 <willmoffat181@gmail.com>
2022-08-28 12:52:17 +0100

    Fixing incorrect project setups

--- Proof/Properties/AssemblyInfo.cs
+++ Proof/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sandbox")]
+[assembly: AssemblyTitle("Proof")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sandbox")]
+[assembly: AssemblyProduct("Proof")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe20d0b3-acba-4569-9fbd-1c38c4d6bbc4")]
+[assembly: Guid("8c1c4546-0690-44e1-8d44-0826e221fd57")]
 
 // Version information for an assembly consists of the following four values:
 //
