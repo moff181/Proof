ffc2e6305bb5ee387d99e3adba0a20d6d24e1c64
will181 <willmoffat181@gmail.com>
2022-08-29 18:32:13 +0100

    Added TransformComponent

---
+++ Proof/Entity/Components/TransformComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace Proof.Entity.Components
+{
+    internal class TransformComponent : IComponent
+    {
+        public TransformComponent(Vector2 position, Vector2 scale)
+        {
+            Position = position;
+            Scale = scale;
+        }
+
+        public TransformComponent()
+            : this(new Vector2(0, 0), new Vector2(0, 0))
+        { }
+
+        public Vector2 Position { get; set; }
+        public Vector2 Scale { get; set; }
+
+        public void Update(Entity entity)
+        { }
+    }
+}
