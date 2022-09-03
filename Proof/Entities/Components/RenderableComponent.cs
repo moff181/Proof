9f6616ce853301ff6902101cef0675ddb68e90b3
will181 <willmoffat181@gmail.com>
2022-09-03 13:01:18 +0100

    Optimising RenderableComponent by only perform vertex transformations when the position or scale of the TransformComponent has changed

--- Proof/Entities/Components/RenderableComponent.cs
+++ Proof/Entities/Components/RenderableComponent.cs
@@ -1,6 +1,7 @@
 ﻿using Proof.Core.Logging;
 using Proof.Render;
 using Proof.Render.Buffers;
+using System.Numerics;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -14,30 +15,37 @@ namespace Proof.Entities.Components
         private readonly Model _model;
         private readonly int _layer;
 
+        private float[] _verticesBuffer;
+        private Vector2 _previousPosition;
+        private Vector2 _previousScale;
+
         public RenderableComponent(Renderer renderer, VertexLayout layout, Model model, int layer)
         {
             _renderer = renderer;
             _layout = layout;
             _model = model;
             _layer = layer;
+
+            _verticesBuffer = new float[model.Vertices.Length];
+            _previousPosition = Vector2.Zero;
+            _previousScale = Vector2.One;
         }
 
         public void Update(Entity entity)
-        {            
-            var vertices = new float[_model.Vertices.Length];
-            Array.Copy(_model.Vertices, vertices, vertices.Length);
-
+        {
             var transformComponent = entity.GetComponent<TransformComponent>();
-            if (transformComponent != null)
+            if (transformComponent != null && (transformComponent.Position != _previousPosition || transformComponent.Scale != _previousScale))
             {
-                for (int i = _layout.PositionIndex; i < vertices.Length; i += _layout.SumOfElements())
+                Array.Copy(_model.Vertices, _verticesBuffer, _verticesBuffer.Length);
+
+                for (int i = _layout.PositionIndex; i < _verticesBuffer.Length; i += _layout.SumOfElements())
                 {
-                    vertices[i] = transformComponent.Scale.X * vertices[i] + transformComponent.Position.X;
-                    vertices[i+1] = transformComponent.Scale.Y * vertices[i+1] + transformComponent.Position.Y;
+                    _verticesBuffer[i] = transformComponent.Scale.X * _verticesBuffer[i] + transformComponent.Position.X;
+                    _verticesBuffer[i+1] = transformComponent.Scale.Y * _verticesBuffer[i+1] + transformComponent.Position.Y;
                 }
             }
 
-            _renderer.Submit(vertices, _model.Indices, _layer);
+            _renderer.Submit(_verticesBuffer, _model.Indices, _layer);
         }
 
         public static IComponent LoadFromNode(
