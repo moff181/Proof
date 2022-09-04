3bc13144224ddf7d4f59678a024d94e664362782
will181 <willmoffat181@gmail.com>
2022-09-04 15:01:00 +0100

    Updated Proof.DevEnv to render proof Window inside the WPF window

--- Proof.DevEnv/MainWindow.xaml.cs
+++ Proof.DevEnv/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Window = System.Windows.Window;
+using ProofWindow = Proof.Render.Window;
+using Proof.Core.Logging;
+using System.Diagnostics;
+using System;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Proof.DevEnv
 {
@@ -20,9 +12,28 @@ namespace Proof.DevEnv
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ProofWindow _window;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Task.Run(ProcessGameEngine);
+        }
+
+        private void ProcessGameEngine()
+        {
+            IntPtr windowHandle;
+            while ((windowHandle = Process.GetCurrentProcess().MainWindowHandle) == IntPtr.Zero)
+            { }
+
+            var logger = new ConsoleLogger();
+            var window = new ProofWindow(logger, 720, 576, "DevEnv", false, windowHandle);
+
+            while(!window.ShouldClose())
+            {
+                window.Update();
+            }
         }
     }
 }
