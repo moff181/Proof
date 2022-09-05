620ae027bc498f29377af0a562cbd02efc86cb69
will181 <willmoffat181@gmail.com>
2022-09-05 22:03:19 +0100

    Added NoLogger

---
+++ Proof/Core/Logging/NoLogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Proof.Core.Logging
+{
+    public class NoLogger : ALogger
+    {
+        protected override void Log(string str)
+        { }
+
+        protected override void SetColour(ConsoleColor color)
+        { }
+    }
+}
