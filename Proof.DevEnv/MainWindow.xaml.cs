b9a38b81ab81dd37bc484cc41d8e97ad28642dd8
will181 <willmoffat181@gmail.com>
2022-09-08 22:23:54 +0100

    Added logic to resize the game window when the editor window is resized

--- Proof.DevEnv/MainWindow.xaml.cs
+++ Proof.DevEnv/MainWindow.xaml.cs
@@ -16,18 +16,37 @@ namespace Proof.DevEnv
         {
             InitializeComponent();
 
-            Task.Run(ProcessGameEngine);
+            int width = (int)Width;
+            int height = (int)Height;
+            Task.Run(() => ProcessGameEngine(width, height));
         }
 
-        private void ProcessGameEngine()
+        private void ProcessGameEngine(int width, int height)
         {
             // This will need updating to not just hardcode
             Directory.SetCurrentDirectory("../../../../Sandbox");
 
             var application = new DevEnvApplication();
             _window = application.Window;
+            SizeGameWindowToEditorWindow(width, height);
 
             application.Run("res/scenes/TestScene.xml");
+        }
+
+        private void Window_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            var newSize = e.NewSize;
+            SizeGameWindowToEditorWindow((int)newSize.Width, (int)newSize.Height);
+        }
+
+        private void SizeGameWindowToEditorWindow(int windowWidth, int windowHeight)
+        {
+            if (_window == null)
+            {
+                return;
+            }
+
+            _window.Resize(windowWidth / 2, windowHeight / 2);
         }
     }
 }
