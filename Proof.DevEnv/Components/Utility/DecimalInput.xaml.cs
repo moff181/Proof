a2bece46ec36b6d9f0b45471f4fa796c68a15451
will181 <willmoffat181@gmail.com>
2022-09-15 12:41:48 +0100

    Added DecimalInput and updating Vector2Component to use it

---
+++ Proof.DevEnv/Components/Utility/DecimalInput.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Proof.DevEnv.Components.Utility
+{
+    public partial class DecimalInput : UserControl
+    {
+        public DecimalInput()
+        {
+            InitializeComponent();
+        }
+
+        public float Value { get { return float.Parse(Input.Text); } set { Input.Text = value.ToString(); } }
+        
+        public Action? OnValueChange { get; set; }
+
+        private void Value_LostFocus(object s, System.Windows.RoutedEventArgs e)
+        {
+            Input.Text = FormatToNumber(Input.Text);
+
+            if(OnValueChange == null)
+            {
+                return;
+            }
+
+            OnValueChange();
+        }
+
+        private void TextBox_PreviewTextInput(object s, TextCompositionEventArgs e)
+        {
+            if (s == null || s is not TextBox)
+            {
+                return;
+            }
+
+            TextBox sender = (TextBox)s;
+
+            string newText = sender.Text.Insert(sender.SelectionStart, e.Text);
+
+            if (newText.Count(c => c == '-') > 1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (newText.Count(c => c == '-') == 1 && !newText.Replace(" ", "").StartsWith('-'))
+            {
+                e.Handled = true;
+                return;
+            }
+
+            var regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
+            e.Handled = !regex.IsMatch(newText.Replace("-", ""));
+        }
+
+        private string FormatToNumber(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return "0";
+            }
+
+            s = s.Replace(" ", "");
+
+            if (s.EndsWith("."))
+            {
+                s += "0";
+            }
+
+            return s;
+        }
+    }
+}
--- Proof.DevEnv/Components/Utility/Vector2Component.xaml.cs
+++ Proof.DevEnv/Components/Utility/Vector2Component.xaml.cs
@@ -21,68 +21,18 @@ namespace Proof.DevEnv.Components.Utility
 
         public void Init(Vector2 val)
         {
-            XValue.Text = val.X.ToString();
-            YValue.Text = val.Y.ToString();
+            XValue.Value = val.X;
+            YValue.Value = val.Y;
         }
 
-        private void Value_LostFocus(object s, System.Windows.RoutedEventArgs e)
+        private void Input_OnValueChange()
         {
-            XValue.Text = FormatToNumber(XValue.Text);
-            YValue.Text = FormatToNumber(YValue.Text);
-
-            if (OnChange == null)
+            if(OnChange == null)
             {
                 return;
             }
 
-            float x = float.Parse(XValue.Text);
-            float y = float.Parse(YValue.Text);
-
-            OnChange(new Vector2(x, y));
-        }
-
-        private void TextBox_PreviewTextInput(object s, TextCompositionEventArgs e)
-        {
-            if(s == null || s is not TextBox)
-            {
-                return;
-            }
-
-            TextBox sender = (TextBox)s;
-
-            string newText = sender.Text.Insert(sender.SelectionStart, e.Text);
-
-            if (newText.Count(c => c == '-') > 1)
-            {
-                e.Handled = true;
-                return;
-            }
-
-            if (newText.Count(c => c == '-') == 1 && !newText.Replace(" ", "").StartsWith('-'))
-            {
-                e.Handled = true;
-                return;
-            }
-
-            var regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
-            e.Handled = !regex.IsMatch(newText.Replace("-", ""));
-        }
-
-        private string FormatToNumber(string s)
-        {
-            if(string.IsNullOrWhiteSpace(s))
-            {
-                return "0";
-            }
-
-            s = s.Replace(" ", "");
-            
-            if(s.EndsWith("."))
-            {
-                s += "0";
-            }
-
-            return s;
+            OnChange(new Vector2(XValue.Value, YValue.Value));
         }
     }
 }
