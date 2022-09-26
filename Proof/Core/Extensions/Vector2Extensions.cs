7289f0918de17bb95766202c6fa1f16cb9e8bd30
will181 <willmoffat181@gmail.com>
2022-09-26 23:06:15 +0100

    Added Vector2Extensions

---
+++ Proof/Core/Extensions/Vector2Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+using System.Xml.Linq;
+
+namespace Proof.Core.Extensions
+{
+    public static class Vector2Extensions
+    {
+        public static XElement ToXml(this Vector2 vec, string elementName)
+        {
+            return new XElement(
+                elementName,
+                new XElement("X", vec.X),
+                new XElement("Y", vec.Y));
+        }
+    }
+}
