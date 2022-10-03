5df8d5fb2294a61c4186b825bd63c12c232db54e
will181 <willmoffat181@gmail.com>
2022-10-03 18:34:34 +0100

    Added Proof.Runner project

---
+++ Proof.Runner/Program.cs
@@ -0,0 +1,56 @@
+﻿using Proof.Core.Logging;
+using Proof.Entities.Components.Scripts;
+using System.Reflection;
+
+namespace Proof.Runner
+{
+    internal static class Program
+    {
+        private const string DllName = "Game.dll";
+        private const string EntryPointTypeName = "Proof.Game.GameApplication";
+        private const string EntryPointMethodName = "Run";
+
+        public static void Main(string[] args)
+        {
+            ALogger logger = new ConsoleLogger();
+
+            try
+            {
+                string directory = Directory.GetCurrentDirectory();
+                string path = Path.Combine(directory, DllName);
+
+                var assembly = Assembly.LoadFile(path);
+
+                Type? gameApplicationType = assembly.GetType(EntryPointTypeName);
+                if (gameApplicationType == null)
+                {
+                    throw new TypeLoadException("Failed to load GameApplication");
+                }
+
+                MethodInfo? entryPoint = gameApplicationType.GetMethod(EntryPointMethodName);
+                if(entryPoint == null)
+                {
+                    throw new TypeLoadException("Failed to find the entry point function.");
+                }
+
+                var gameApplication = gameApplicationType.GetConstructors().First().Invoke(new object[] { logger });
+                entryPoint.Invoke(gameApplication, new object[] { "res/scenes/TestScene.xml" });
+            }
+            catch(Exception e)
+            {
+                logger.LogError("Failed to create game instance", e);
+                return;
+            }
+        }
+    }
+}
+
+namespace Proof.Game
+{
+    internal class GameApplication : Application
+    {
+        public GameApplication(ALogger logger)
+            : base(logger, "Proof Game", new ScriptLoader(Assembly.GetExecutingAssembly(), logger))
+        { }
+    }
+}
