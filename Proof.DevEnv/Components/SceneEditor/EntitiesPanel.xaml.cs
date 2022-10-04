72e270e4ffa751c65b4631231db909724c0b2195
will181 <willmoffat181@gmail.com>
2022-10-04 13:18:16 +0100

    Adding support for adding a new entity within the DevEnv

--- Proof.DevEnv/Components/SceneEditor/EntitiesPanel.xaml.cs
+++ Proof.DevEnv/Components/SceneEditor/EntitiesPanel.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using Proof.Entities;
 using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Proof.DevEnv.Components
 {
     public partial class EntitiesPanel : UserControl
     {
+        private Scene? _scene;
+        private Action<Entity>? _onClick;
+
         public EntitiesPanel()
         {
             InitializeComponent();
@@ -13,6 +18,9 @@ namespace Proof.DevEnv.Components
 
         public void Init(Scene scene, Action<Entity> onClick)
         {
+            _scene = scene;
+            _onClick = onClick;
+
             Body.Children.Clear();
 
             foreach(Entity entity in scene.Entities)
@@ -24,6 +32,28 @@ namespace Proof.DevEnv.Components
                 button.Click += (sender, e) => onClick(entity);
 
                 Body.Children.Add(button);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_scene == null || _onClick == null)
+            {
+                return;
+            }
+
+            string name = NewEntityNameBox.Text;
+            _scene.Entities.Add(new Entity(name));
+
+            NewEntityNameBox.Text = string.Empty;
+            Init(_scene, _onClick);
+        }
+
+        private void NewEntityNameBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Return)
+            {
+                Button_Click(sender, new RoutedEventArgs());
             }
         }
     }
