6da75998f04a4894039ddff483222f3f9fd354b7
will181 <willmoffat181@gmail.com>
2022-10-05 23:34:00 +0100

    Adding missing dependency from 9daea37 (FileLogger)

---
+++ Proof/Core/Logging/FileLogger.cs
@@ -0,0 +1,22 @@
+﻿namespace Proof.Core.Logging
+{
+    public class FileLogger : ALogger
+    {
+        private readonly string _fileName;
+
+        public FileLogger(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        protected override void Log(string str)
+        {
+            File.AppendAllText(_fileName, $"{str}\n");
+        }
+
+        protected override void SetColour(ConsoleColor color)
+        {
+            // Colour not supported for file logger
+        }
+    }
+}
