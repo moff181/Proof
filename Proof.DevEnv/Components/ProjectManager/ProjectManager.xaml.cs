7af3ef2027d3d5cb6c4291dde9ac243a8e1337be
will181 <willmoffat181@gmail.com>
2022-10-05 21:43:16 +0100

    Updating ProjectManager to default containing folder to desktop

--- Proof.DevEnv/Components/ProjectManager/ProjectManager.xaml.cs
+++ Proof.DevEnv/Components/ProjectManager/ProjectManager.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Forms;
 using UserControl = System.Windows.Controls.UserControl;
@@ -14,13 +15,13 @@ namespace Proof.DevEnv.Components.ProjectManager
         {
             InitializeComponent();
 
-            using var dialog = new FolderBrowserDialog();
-            ContainingFolderText.Text = dialog.SelectedPath;
+            ContainingFolderText.Text = GetDesktopPath();
         }
 
         private void SelectFolder_Click(object sender, RoutedEventArgs e)
         {
             using var dialog = new FolderBrowserDialog();
+            dialog.InitialDirectory = ContainingFolderText.Text;
             DialogResult result = dialog.ShowDialog();
 
             if(result != DialogResult.OK)
@@ -41,6 +42,11 @@ namespace Proof.DevEnv.Components.ProjectManager
             Directory.CreateDirectory(Path.Combine(directory, "res", "models"));
             Directory.CreateDirectory(Path.Combine(directory, "res", "scenes"));
             Directory.CreateDirectory(Path.Combine(directory, "res", "shaders"));
+        }
+
+        private string GetDesktopPath()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
         }
     }
 }
