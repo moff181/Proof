35914796c92fdf052221ac155ed41fd5891d6a71
will181 <willmoffat181@gmail.com>
2022-10-08 20:09:00 +0100

    Added a basic toolbar with functionality not implemented yet

---
+++ Proof.DevEnv/Components/SceneEditor/Toolbar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Proof.DevEnv.Components
+{
+    public partial class Toolbar : UserControl
+    {
+        public Toolbar()
+        {
+            InitializeComponent();
+        }
+    }
+}
