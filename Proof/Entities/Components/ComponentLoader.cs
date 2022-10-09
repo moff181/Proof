0745e87bea015a82ad9a60ff8a967575f2fa1a3d
will181 <willmoffat181@gmail.com>
2022-10-09 18:08:09 +0100

    Added ColourComponent to ComponentLoader

--- Proof/Entities/Components/ComponentLoader.cs
+++ Proof/Entities/Components/ComponentLoader.cs
@@ -33,12 +33,14 @@ namespace Proof.Entities.Components
             {
                 case "CameraComponent":
                     return CameraComponent.LoadFromNode(_logger, shader, componentNode);
+                case "ColourComponent":
+                    return ColourComponent.LoadFromNode(componentNode);
                 case "RenderableComponent":
                     return RenderableComponent.LoadFromNode(_logger, modelLibrary, renderer, layout, componentNode);
-                case "TransformComponent":
-                    return TransformComponent.LoadFromNode(_logger, componentNode);
                 case "ScriptComponent":
                     return ScriptComponent.LoadFromXml(componentNode, scriptLoader, inputManager);
+                case "TransformComponent":
+                    return TransformComponent.LoadFromNode(_logger, componentNode);
                 default:
                     throw new NotSupportedException($"Unable to load component node with name: {componentNode}");
             }
