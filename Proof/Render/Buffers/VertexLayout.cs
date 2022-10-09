3c6ef7b87ff3466f4153eddb21f2b18244adcdc9
will181 <willmoffat181@gmail.com>
2022-10-09 17:57:00 +0100

    Added ColourIndex to VertexLayout

--- Proof/Render/Buffers/VertexLayout.cs
+++ Proof/Render/Buffers/VertexLayout.cs
@@ -10,13 +10,15 @@ namespace Proof.Render.Buffers
         private readonly IList<int> _arraySizes;
         private int _sumOfElements;
 
-        public VertexLayout(int positionIndex)
+        public VertexLayout(int positionIndex, int? colourIndex)
         {
             _arraySizes = new List<int>();
             PositionIndex = positionIndex;
+            ColourIndex = colourIndex;
         }
 
         public int PositionIndex { get; }
+        public int? ColourIndex { get; }
 
         public void AddArray(int arraySize)
         {
@@ -52,17 +54,24 @@ namespace Proof.Render.Buffers
         public static VertexLayout LoadFromNode(ALogger logger, XElement vertexLayoutNode)
         {
             XElement? positionIndexNode = vertexLayoutNode.Element("PositionIndex");
-            if(positionIndexNode == null)
+            if (positionIndexNode == null)
             {
                 throw new XmlException("Could not find PositionIndex node in vertex layout file.");
             }
 
-            if(!int.TryParse(positionIndexNode.Value, out int positionIndex))
+            if (!int.TryParse(positionIndexNode.Value, out int positionIndex))
             {
                 throw new XmlException($"Value stored in PositionIndex node was invalid: {positionIndexNode.Value}");
             }
 
-            var vertexLayout = new VertexLayout(positionIndex);
+            int? colourIndex = null;
+            XElement? colourIndexNode = vertexLayoutNode.Element("ColourIndex");
+            if (colourIndexNode != null && int.TryParse(colourIndexNode.Value, out int colourIndexTemp))
+            {
+                colourIndex = colourIndexTemp;
+            }
+
+            var vertexLayout = new VertexLayout(positionIndex, colourIndex);
 
             XElement? attribArraysNode = vertexLayoutNode.Element("AttribArrays");
             if(attribArraysNode == null)
