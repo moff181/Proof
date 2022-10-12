f2e334e5b2e19ab9bf26f76f0af032252e61537f
will181 <willmoffat181@gmail.com>
2022-10-12 20:48:22 +0100

    Added ChangeHistory class

---
+++ Proof.DevEnv/ChangeHistory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Proof.DevEnv
+{
+    public class ChangeHistory
+    {
+        private readonly Action _onUnsavedChange;
+        private bool _unsavedChange;
+
+        public ChangeHistory(Action onUnsavedChange)
+        {
+            _onUnsavedChange = onUnsavedChange;
+        }
+        
+        public void RegisterChange()
+        {
+            if(_unsavedChange)
+            {
+                return;
+            }
+
+            _unsavedChange = true;
+            _onUnsavedChange();
+        }
+
+        public void RegisterSave()
+        {
+            _unsavedChange = false;
+        }
+    }
+}
