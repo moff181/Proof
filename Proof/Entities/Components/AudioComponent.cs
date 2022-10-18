50769d7e451a7e7d367a3271557fb679d6034946
will181 <willmoffat181@gmail.com>
2022-10-18 20:48:31 +0100

    Making sound public in AudioComponent

---
+++ Proof/Entities/Components/AudioComponent.cs
@@ -0,0 +1,25 @@
+﻿using Proof.Audio;
+using System.Xml.Linq;
+
+namespace Proof.Entities.Components
+{
+    public class AudioComponent : IComponent
+    {
+        public AudioComponent(Sound sound)
+        {
+            Sound = sound;
+        }
+
+        public Sound Sound { get; }
+
+        public void Update(Entity entity)
+        {
+            // Do nothing
+        }
+
+        public XElement ToXml()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
