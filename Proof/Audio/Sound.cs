2bd6bd9fe8e15a2a618a31ea921a54ffa4b3a060
will181 <willmoffat181@gmail.com>
2022-10-19 18:17:03 +0100

    Updating Sound to use NAudio

--- Proof/Audio/Sound.cs
+++ Proof/Audio/Sound.cs
@@ -1,37 +1,59 @@
-﻿using Proof.Core.Logging;
-using System.Media;
-using System.Runtime.Versioning;
+﻿using NAudio.Wave;
+using Proof.Core.Logging;
 
 namespace Proof.Audio
 {
-    [SupportedOSPlatform("windows")]
-    public class Sound
+    public sealed class Sound : IDisposable
     {
-        private readonly SoundPlayer _soundPlayer;
+        private readonly ALogger _logger;
+        private readonly WaveFileReader _waveReader;
+        private readonly WaveOutEvent _wave;
 
         internal Sound(ALogger logger, string path)
         {
-            logger.LogInfo($"Loading sound from {path}");
+            _logger = logger;
+
+            _logger.LogInfo($"Loading sound from {path}...");
+
             Path = path;
-            _soundPlayer = new SoundPlayer(path);
-            logger.LogInfo("Sound loaded.");
+
+            _waveReader = new WaveFileReader(path);
+            _wave = new WaveOutEvent();
+            _wave.Init(_waveReader);
+
+            _logger.LogInfo("Sound loaded.");
+        }
+
+        ~Sound()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _logger.LogInfo($"Disposing of Sound: {Path}");
+
+            _wave.Dispose();
+            _waveReader.Dispose();
+
+            _logger.LogInfo("Sound disposed of successfully.");
         }
 
         public string Path { get; }
 
         public void Play()
         {
-            _soundPlayer.Play();
-        }
-
-        public void Loop()
-        {
-            _soundPlayer.PlayLooping();
+            _wave.Play();
         }
 
         public void Stop()
         {
-            _soundPlayer.Stop();
+            _wave.Stop();
+        }
+
+        public void Pause()
+        {
+            _wave.Pause();
         }
     }
 }
