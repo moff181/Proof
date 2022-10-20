5e84e37be97a92a4f7dad3bcea68c33b880d975d
will181 <willmoffat181@gmail.com>
2022-10-20 20:33:22 +0100

    Implementing TextureLibrary

--- Proof/Render/Textures/TextureLibrary.cs
+++ Proof/Render/Textures/TextureLibrary.cs
@@ -1,6 +1,28 @@
-﻿namespace Proof.Render.Textures
+﻿using Proof.Core.Logging;
+using System.Runtime.Versioning;
+
+namespace Proof.Render.Textures
 {
     public class TextureLibrary
     {
+        private readonly ALogger _logger;
+        private readonly Dictionary<string, Texture> _textures;
+
+        public TextureLibrary(ALogger logger)
+        {
+            _logger = logger;
+            _textures = new Dictionary<string, Texture>();
+        }
+
+        [SupportedOSPlatform("windows")]
+        public Texture Get(string path)
+        {
+            if(_textures.TryGetValue(path, out Texture? result))
+            {
+                return result;
+            }
+
+            return new Texture(_logger, path);
+        }
     }
 }
