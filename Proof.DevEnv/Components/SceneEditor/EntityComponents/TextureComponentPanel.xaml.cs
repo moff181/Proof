9cce366473dd8cf76ca17f4b87f4654136cda1f1
will181 <willmoffat181@gmail.com>
2022-10-20 23:17:51 +0100

    Fixing issue where TextureComponentPanel would default text box to empty string

--- Proof.DevEnv/Components/SceneEditor/EntityComponents/TextureComponentPanel.xaml.cs
+++ Proof.DevEnv/Components/SceneEditor/EntityComponents/TextureComponentPanel.xaml.cs
@@ -19,6 +19,8 @@ namespace Proof.DevEnv.Components.EntityComponents
         {
             InitializeComponent();
 
+            TexturePath.Text = textureComponent.Texture.FilePath;
+
             _textureComponent = textureComponent;
             _changeHistory = changeHistory;
             _onRemove = onRemove;
