dd79c222055c978dff8f26c4503d009d5686bbe5
will181 <willmoffat181@gmail.com>
2022-10-20 22:49:15 +0100

    Added TextureComponent

---
+++ Proof/Entities/Components/TextureComponent.cs
@@ -0,0 +1,45 @@
+﻿using Proof.Audio;
+using Proof.Entities.Components.Scripts;
+using Proof.Input;
+using Proof.Render.Textures;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Proof.Entities.Components
+{
+    public class TextureComponent : IComponent
+    {
+        public TextureComponent(Texture texture)
+        {
+            Texture = texture;
+        }
+
+        public Texture Texture { get; set; }
+
+        public void Update(Entity entity)
+        {
+            // Do nothing
+        }
+
+        public XElement ToXml()
+        {
+            return new XElement(
+                "TextureComponent",
+                new XElement("FilePath", Texture.FilePath));
+        }
+
+        public static TextureComponent LoadFromXml(TextureLibrary textureLibrary, XElement componentNode)
+        {
+            XElement? filePathNode = componentNode.Element("FilePath");
+            if (filePathNode == null)
+            {
+                throw new XmlException("Could not find FilePath node for TextureComponent.");
+            }
+
+            string filePath = filePathNode.Value;
+            Texture texture = textureLibrary.Get(filePath);
+
+            return new TextureComponent(texture);
+        }
+    }
+}
