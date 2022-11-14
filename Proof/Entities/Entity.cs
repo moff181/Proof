6dfae6d879e8ab48e8615c0664030223de23ddae
will181 <willmoffat181@gmail.com>
2022-11-14 15:41:06 +0000

    Updating Entity to support multiple instances of specific components

--- Proof/Entities/Entity.cs
+++ Proof/Entities/Entity.cs
@@ -15,11 +15,18 @@ namespace Proof.Entities
 {
     public class Entity
     {
-        private readonly Dictionary<Type, IComponent> _components;
+        private List<AudioComponent> _audioComponent;
+        private CameraComponent? _cameraComponent;
+        private ColourComponent? _colourComponent;
+        private RenderableComponent? _renderableComponent;
+        private List<ScriptComponent> _scriptComponent;
+        private TextureComponent? _textureComponent;
+        private TransformComponent? _transformComponent;
 
         public Entity(string name)
         {
-            _components = new Dictionary<Type, IComponent>();
+            _audioComponent = new List<AudioComponent>();
+            _scriptComponent = new List<ScriptComponent>();
             Name = name;
         }
 
@@ -27,35 +34,148 @@ namespace Proof.Entities
 
         public void Update()
         {
-            foreach (IComponent current in _components.Values)
-            {
-                current.Update(this);
-            }
+            _audioComponent.ForEach(x => x.Update(this));
+            _cameraComponent?.Update(this);
+            _colourComponent?.Update(this);
+            _renderableComponent?.Update(this);
+            _scriptComponent.ForEach(x => x.Update(this));
+            _textureComponent?.Update(this);
+            _transformComponent?.Update(this);
         }
 
         public void AddComponent(IComponent component)
         {
-            _components.Add(component.GetType(), component);
+            if (component is AudioComponent audioComp)
+            {
+                _audioComponent.Add(audioComp);
+            }
+            else if (component is CameraComponent cameraComp)
+            {
+                _cameraComponent = cameraComp;
+            }
+            else if (component is ColourComponent colourComp)
+            {
+                _colourComponent = colourComp;
+            }
+            else if (component is RenderableComponent renderableComp)
+            {
+                _renderableComponent = renderableComp;
+            }
+            else if (component is ScriptComponent scriptComp)
+            {
+                _scriptComponent.Add(scriptComp);
+            }
+            else if (component is TextureComponent textureComp)
+            {
+                _textureComponent = textureComp;
+            }
+            else if (component is TransformComponent transformComp)
+            {
+                _transformComponent = transformComp;
+            }
+            else
+            {
+                throw new ArgumentException($"Unknown component type could not be added.");
+            }
         }
 
         public void RemoveComponent(IComponent component)
         {
-            _components.Remove(component.GetType());
+            if (component is AudioComponent audioComp)
+            {
+                _audioComponent.Remove(audioComp);
+            }
+            else if (component is CameraComponent)
+            {
+                _cameraComponent = null;
+            }
+            else if (component is ColourComponent)
+            {
+                _colourComponent = null;
+            }
+            else if (component is RenderableComponent)
+            {
+                _renderableComponent = null;
+            }
+            else if (component is ScriptComponent scriptComp)
+            {
+                _scriptComponent.Remove(scriptComp);
+            }
+            else if (component is TextureComponent)
+            {
+                _textureComponent = null;
+            }
+            else if (component is TransformComponent)
+            {
+                _transformComponent = null;
+            }
+            else
+            {
+                throw new ArgumentException($"Unknown component type could not be added.");
+            }
         }
 
-        public T? GetComponent<T>()
+        public T? GetComponent<T>() where T : IComponent
         {
-            if(_components.TryGetValue(typeof(T), out IComponent? component))
+            if (typeof(T) == typeof(AudioComponent))
             {
-                return (T)component;
+                return (T?)Convert.ChangeType(_audioComponent.FirstOrDefault(), typeof(T?));
+            }
+            if (typeof(T) == typeof(CameraComponent))
+            {
+                return (T?)Convert.ChangeType(_cameraComponent, typeof(T?));
+            }
+            if (typeof(T) == typeof(ColourComponent))
+            {
+                return (T?)Convert.ChangeType(_colourComponent, typeof(T?));
+            }
+            if (typeof(T) == typeof(RenderableComponent))
+            {
+                return (T?)Convert.ChangeType(_renderableComponent, typeof(T?));
+            }
+            if (typeof(T) == typeof(ScriptComponent))
+            {
+                return (T?)Convert.ChangeType(_scriptComponent.FirstOrDefault(), typeof(T?));
+            }
+            if (typeof(T) == typeof(TextureComponent))
+            {
+                return (T?)Convert.ChangeType(_textureComponent, typeof(T?));
+            }
+            if (typeof(T) == typeof(TransformComponent))
+            {
+                return (T?)Convert.ChangeType(_transformComponent, typeof(T?));
             }
 
-            return default(T);
+            throw new ArgumentException("Unknown type for retrieving component");
+        }
+
+        public AudioComponent? GetAudio(string path)
+        {
+            return _audioComponent.FirstOrDefault(x => x.Sound.Path == path);
+        }
+
+        public ScriptComponent? GetScript(string scriptName)
+        {
+            return _scriptComponent.FirstOrDefault(x => x.ScriptName == scriptName);
         }
 
         public IEnumerable<IComponent> GetComponents()
         {
-            return _components.Values;
+            // TODO: update this to work with attributes or something
+            var result = new List<IComponent?>
+            {
+                _cameraComponent,
+                _colourComponent,
+                _renderableComponent,
+                _textureComponent,
+                _transformComponent,
+            };
+            result.AddRange(_audioComponent);
+            result.AddRange(_scriptComponent);
+
+            return result
+                .Where(x => x != null)
+                .Cast<IComponent>();
         }
 
         public XElement ToXml()
