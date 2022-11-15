cbcfd4e6c5d112283a7f035db873b0b1252f51cf
will181 <willmoffat181@gmail.com>
2022-11-15 21:02:51 +0000

    Updated Texture to use IOpenGLApi and GLConstants (missed items)

--- Proof/Render/Textures/Texture.cs
+++ Proof/Render/Textures/Texture.cs
@@ -18,13 +18,13 @@ namespace Proof.Render.Textures
 
             logger.LogInfo($"Loading texture from file: {filePath}");
 
-            _textureId = GL.glGenTextures(1)[0];
-            _gl.BindTexture(GL.GL_TEXTURE_2D, _textureId);
+            _textureId = _gl.GenTextures(1)[0];
+            _gl.BindTexture(GLConstants.GL_TEXTURE_2D, _textureId);
 
-            _gl.TexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR);
-            _gl.TexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
-            _gl.TexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP_TO_EDGE);
-            _gl.TexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP_TO_EDGE);
+            _gl.TexParameteri(GLConstants.GL_TEXTURE_2D, GLConstants.GL_TEXTURE_MIN_FILTER, GLConstants.GL_LINEAR);
+            _gl.TexParameteri(GLConstants.GL_TEXTURE_2D, GLConstants.GL_TEXTURE_MAG_FILTER, GLConstants.GL_LINEAR);
+            _gl.TexParameteri(GLConstants.GL_TEXTURE_2D, GLConstants.GL_TEXTURE_WRAP_S, GLConstants.GL_CLAMP_TO_EDGE);
+            _gl.TexParameteri(GLConstants.GL_TEXTURE_2D, GLConstants.GL_TEXTURE_WRAP_T, GLConstants.GL_CLAMP_TO_EDGE);
 
             PngImage image = PngLoader.LoadImage(filePath);
 
@@ -32,14 +32,14 @@ namespace Proof.Render.Textures
             Marshal.Copy(image.Data, 0, ptr, image.Data.Length);
 
             _gl.TexImage2D(
-                GL.GL_TEXTURE_2D,
+                GLConstants.GL_TEXTURE_2D,
                 0,
-                GL.GL_RGB8,
+                GLConstants.GL_RGB8,
                 image.CoreImageData.Width,
                 image.CoreImageData.Height,
                 0,
-                GL.GL_RGB,
-                GL.GL_UNSIGNED_BYTE,
+                GLConstants.GL_RGB,
+                GLConstants.GL_UNSIGNED_BYTE,
                 ptr);
 
             logger.LogInfo($"Loaded texture successfully.");
@@ -49,8 +49,8 @@ namespace Proof.Render.Textures
 
         public void Bind(int textureSlot)
         {
-            _gl.ActiveTexture(GL.GL_TEXTURE0 + textureSlot);
-            _gl.BindTexture(GL.GL_TEXTURE_2D, _textureId);
+            _gl.ActiveTexture(GLConstants.GL_TEXTURE0 + textureSlot);
+            _gl.BindTexture(GLConstants.GL_TEXTURE_2D, _textureId);
         }
     }
 }
