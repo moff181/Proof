99b92f0878d54a46f15808ff49badbc19b794b8d
will181 <willmoffat181@gmail.com>
2022-11-15 17:55:13 +0000

    Added wrapper class for GL that isn't static (to allow for dependency injection)

--- Proof.Runner/Program.cs
+++ Proof.Runner/Program.cs
@@ -1,7 +1,5 @@
-﻿using Proof.Core.Images;
-using Proof.Core.Logging;
+﻿using Proof.Core.Logging;
 using Proof.Core.ProjectStructure;
-using Proof.Entities.Components.Scripts;
 using System.Reflection;
 
 namespace Proof.Runner
