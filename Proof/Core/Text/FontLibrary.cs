9633be055686d070f87f400017b67a3974faa190
will181 <willmoffat181@gmail.com>
2022-11-17 21:34:42 +0000

    Added FontLibrary

---
+++ Proof/Core/Text/FontLibrary.cs
@@ -0,0 +1,29 @@
+﻿using Proof.Render.Textures;
+
+namespace Proof.Core.Text
+{
+    public class FontLibrary
+    {
+        private readonly Dictionary<string, Font> _fonts;
+        private readonly TextureLibrary _textureLibrary;
+
+        public FontLibrary(TextureLibrary textureLibrary)
+        {
+            _fonts = new Dictionary<string, Font>();
+            _textureLibrary = textureLibrary;
+        }
+
+        public Font Get(string filePath)
+        {
+            if(_fonts.TryGetValue(filePath, out Font? font))
+            {
+                return font;
+            }
+
+            var newFont = Font.LoadFromFile(filePath, _textureLibrary);
+            _fonts.Add(filePath, newFont);
+
+            return newFont;
+        }
+    }
+}
